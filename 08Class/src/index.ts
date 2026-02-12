7f293e1f6e00486149ba8991b42b67d34981c1e3
Smith-dev546 <smithmendez098@gmail.com>
2026-02-12 10:10:19 -0600

    Agregando clases de parametros

---
+++ 08Class/src/index.ts
@@ -0,0 +1,23 @@
+class Inventario{
+    id:number;
+    producto: string;
+    codigo: string;
+    fecha: string;
+    precio: number;
+
+    constructor(id:number, producto:string, codigo:string, fecha:string, precio:number){
+        this.id = id;
+        this.producto = producto;
+        this.codigo = codigo;
+        this.fecha = fecha;
+        this.precio = precio
+    }
+
+    Agregar(precio2: number, precio3: number): void{
+        console.log(`Producto agregado con precio: $${this.precio}`);
+        console.log(`Precios de descuento: $${precio2}  y $${precio3}`)
+    }
+}
+
+let producto = new Inventario(1,'Toallas humedas', "psdm32",'2026/02/12', 2.5);
+producto.Agregar(2.45, 2.40);
